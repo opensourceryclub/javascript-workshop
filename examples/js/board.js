54eb535a7dd081183b648ff4a13c698a39c5d019
Don Isaac <donald.isaac@gmail.com>
2020-02-09 15:36:04 -0500

    stuff with the board

--- examples/js/board.js
+++ examples/js/board.js
@@ -1,4 +1,26 @@
-export const BLOCK_SIZE = 25;
+
+class Board {
+
+  static BLOCK_SIZE = 25;
+  width;
+  height;
+  _ctx;
+
+  constructor() {
+    const canvas  = document.getElementById('snake-canvas');
+    this._ctx     = canvas.getContext('2d');
+    this.width    = canvas.width;
+    this.height   = canvas.height;
+  }
+
+  refresh() {
+    this._ctx.clearRect(0, 0, this.width, this.height);
+  }
+
+  drawCell(...cells) {
+
+  }
+}
 
 /* a function to convert row and column to a location object containing x and y in px */
 export function getLocation(row, column) {
